48a392058140fd1f52c1f21ce2e3ca7f831de158
Enzo Pacheco <enzo.pach@gmail.com>
2025-09-15 16:45:11 -0300

    finalizando o evento

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ function verificarLogin(event) {
 
     const usuarios = [
         { nome: "teste", email: "teste@teste.com", senha: "1234" },
-        { nome: "Paulo Baptista", email: "paulo@appavaliaai.com", senha: "Pb1234@" }, //SOPROS
-        { nome: "Marcelo Bambam", email: "marcelo@appavaliaai.com", senha: "Mb1324#" }, //SOPROS
-        // { nome: "Felipe Sangali", email: "felipe@appavaliaai.com", senha: "Fs1432#" }, //SOPROS
-        { nome: "Marco Almeida Jr", email: "marco@appavaliaai.com", senha: "Ma1325#" }, //INTERPRETAÇÃO
-        { nome: "Jorge Scheffer", email: "jorge@appavaliaai.com", senha: "Js5678#" }, //INTERPRETAÇÃO
-        { nome: "Hércules Alves", email: "hercules@appavaliaai.com", senha: "Ha0987#" }, //PERCUSSÃO
-        { nome: "Luiz Caldana", email: "luiz@appavaliaai.com", senha: "Lc0990#" }, //PERCUSSÃO
-        // { nome: "Celso Ribeiro", email: "celso@appavaliaai.com", senha: "Cr1675#" }, //PERCUSSÃO
+        // { nome: "Paulo Baptista", email: "paulo@appavaliaai.com", senha: "Pb1234@" }, //SOPROS
+        // { nome: "Marcelo Bambam", email: "marcelo@appavaliaai.com", senha: "Mb1324#" }, //SOPROS
+        // // { nome: "Felipe Sangali", email: "felipe@appavaliaai.com", senha: "Fs1432#" }, //SOPROS
+        // { nome: "Marco Almeida Jr", email: "marco@appavaliaai.com", senha: "Ma1325#" }, //INTERPRETAÇÃO
+        // { nome: "Jorge Scheffer", email: "jorge@appavaliaai.com", senha: "Js5678#" }, //INTERPRETAÇÃO
+        // { nome: "Hércules Alves", email: "hercules@appavaliaai.com", senha: "Ha0987#" }, //PERCUSSÃO
+        // { nome: "Luiz Caldana", email: "luiz@appavaliaai.com", senha: "Lc0990#" }, //PERCUSSÃO
+        // // { nome: "Celso Ribeiro", email: "celso@appavaliaai.com", senha: "Cr1675#" }, //PERCUSSÃO
 
-        { nome: "Eliane Humberg", email: "eliane@appavaliaai.com", senha: "Eh0009@" }, //UNIFORMIDADE E INSTRUMENTAL
-        { nome: "Celso Ribeiro", email: "celso@appavaliaai.com", senha: "Cr1675#" }, //COBERTURA
-        { nome: "Felipe Sangali", email: "felipe@appavaliaai.com", senha: "Fs1432#" }, //GARBO
-        { nome: "Antônio Carlos Schmidt", email: "schmidth@appavaliaai.com", senha: "Acs0120#" }, //MARCHA
-        { nome: "Airton Moreira", email: "airton@appavaliaai.com", senha: "Am1440#" }, //ALINHAMENTO
+        // { nome: "Eliane Humberg", email: "eliane@appavaliaai.com", senha: "Eh0009@" }, //UNIFORMIDADE E INSTRUMENTAL
+        // { nome: "Celso Ribeiro", email: "celso@appavaliaai.com", senha: "Cr1675#" }, //COBERTURA
+        // { nome: "Felipe Sangali", email: "felipe@appavaliaai.com", senha: "Fs1432#" }, //GARBO
+        // { nome: "Antônio Carlos Schmidt", email: "schmidth@appavaliaai.com", senha: "Acs0120#" }, //MARCHA
+        // { nome: "Airton Moreira", email: "airton@appavaliaai.com", senha: "Am1440#" }, //ALINHAMENTO
 
-        { nome: "Vanessa Vialta", email: "vanessa@appavaliaai.com", senha: "Vv0132@" }, //PELOTÃO CÍVICO
-        { nome: "Júlia Nascimento", email: "julia@appavaliaai.com", senha: "Jn0780@" }, //BALIZA FEMININO
-        { nome: "Karen Lavínia", email: "karen@appavaliaai.com", senha: "Kl3678@" }, //BALIZA MASCULINO
-        { nome: "Débora Elizabeth", email: "debora@appavaliaai.com", senha: "De2043@" }, //CORPO COREOGRÁFICO - TÉCNICO
-        { nome: "Eduardo Caetano", email: "eduardo@appavaliaai.com", senha: "Ec0675@" }, //CORPO COREOGRÁFICO - ARTÍSTICO
-        { nome: "Theo Ribeiro", email: "theo@appavaliaai.com", senha: "Tr9806@" }, //MOR
-        { nome: "Guilherme Souza", email: "guilherme@appavaliaai.com", senha: "Gs6091@" }, //CHECK LIST checkin
+        // { nome: "Vanessa Vialta", email: "vanessa@appavaliaai.com", senha: "Vv0132@" }, //PELOTÃO CÍVICO
+        // { nome: "Júlia Nascimento", email: "julia@appavaliaai.com", senha: "Jn0780@" }, //BALIZA FEMININO
+        // { nome: "Karen Lavínia", email: "karen@appavaliaai.com", senha: "Kl3678@" }, //BALIZA MASCULINO
+        // { nome: "Débora Elizabeth", email: "debora@appavaliaai.com", senha: "De2043@" }, //CORPO COREOGRÁFICO - TÉCNICO
+        // { nome: "Eduardo Caetano", email: "eduardo@appavaliaai.com", senha: "Ec0675@" }, //CORPO COREOGRÁFICO - ARTÍSTICO
+        // { nome: "Theo Ribeiro", email: "theo@appavaliaai.com", senha: "Tr9806@" }, //MOR
+        // { nome: "Guilherme Souza", email: "guilherme@appavaliaai.com", senha: "Gs6091@" }, //CHECK LIST checkin
 
-        { nome: "Checkin", email: "checkin@appavaliaai.com", senha: "Ck1234#" }, //CHECK IN
-        { nome: "Cronômetro", email: "cronometro@appavaliaai.com", senha: "Cr1234#" }, //CRONOMETRO
-        { nome: "Faixa Etária", email: "faixa@appavaliaai.com", senha: "Fe1234#" }, // FAIXA ETARIA
-        { nome: "Checklist", email: "checklist@appavaliaai.com", senha: "Cl1234#" }, // CHECK LIST
-        { nome: "Dados", email: "dados@appavaliaai.com", senha: "1234" }
+        // { nome: "Checkin", email: "checkin@appavaliaai.com", senha: "Ck1234#" }, //CHECK IN
+        // { nome: "Cronômetro", email: "cronometro@appavaliaai.com", senha: "Cr1234#" }, //CRONOMETRO
+        // { nome: "Faixa Etária", email: "faixa@appavaliaai.com", senha: "Fe1234#" }, // FAIXA ETARIA
+        // { nome: "Checklist", email: "checklist@appavaliaai.com", senha: "Cl1234#" }, // CHECK LIST
+        // { nome: "Dados", email: "dados@appavaliaai.com", senha: "1234" }
     ];
 
     const email = document.getElementById("email").value;
